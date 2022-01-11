e69f43ea399d30d5fe39a20f99338b2f620ebf30
Yevhen Pechurin <yevhen.pechurin@gmail.com>
2022-01-11 13:11:13 +0200

    [DEV] adding new futures

--- library/wizards/wizard_book_on_hand.py
+++ library/wizards/wizard_book_on_hand.py
@@ -17,15 +17,15 @@ class WizardOnHand(models.TransientModel):
             'partner_id': self.partner_id.id,
             'due_date': self.due_date,
             'status': 'on_hand',
-        })
-        self.env['library.history'].create({
-            'book_id': book.id,
-            'partner_id': self.partner_id.id,
-            'date_on_hand': today,
-            'due_date': self.due_date,
+            'history_ids': [(0, 0, {
+                'book_id': book.id,
+                'partner_id': self.partner_id.id,
+                'date_on_hand': today,
+                'due_date': self.due_date,
+            })]
         })
 
-    # @api.constrains('due_date')
-    # def constrain_due_date(self):
-    #     if self.due_date <= fields.Date.today():
-    #         raise ValidationError('Вы не можете выбрать такую дату')
+    @api.constrains('due_date')
+    def constrain_due_date(self):
+        if self.due_date <= fields.Date.today():
+            raise ValidationError('Вы не можете выбрать такую дату')
