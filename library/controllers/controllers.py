20f554f3c7e504af0820240266d432c083bd835d
Ievgen <sizority@gmail.com>
2022-01-12 23:31:23 +0200

    Add book in contacts

--- library/controllers/controllers.py
+++ library/controllers/controllers.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Library(http.Controller):
-#     @http.route('/library/library', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Library(http.Controller):
 
-#     @http.route('/library/library/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('library.listing', {
-#             'root': '/library/library',
-#             'objects': http.request.env['library.library'].search([]),
-#         })
+    @http.route('/library/library', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/library/library/objects/<model("library.library"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('library.object', {
-#             'object': obj
-#         })
+    @http.route('/library/books', auth='public', website=True)
+    def list(self, **kw):
+        return http.request.render('library.listing', {
+            'root': '/library',
+            'objects': http.request.env['library.book'].search([]),
+        })
+
+    @http.route('/library/book/<model("library.book"):obj>', auth='public', website=True)
+    def object(self, obj, **kw):
+        return http.request.render('library.object', {
+            'object': obj
+        })
--- library/models/res_partner.py
+++ library/models/res_partner.py
@@ -1,32 +1,27 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
+from odoo import models, fields, api, _
 
 
-class BooksAtClient(models.Model):
-    # _name = 'clients_book'
+class ResPartner(models.Model):
     _inherit = 'res.partner'
-    # _description = 'Book clients'
 
-    book_order_count = fields.Integer(compute='_compute_book_count', string='Book Order Count')
+    book_count = fields.Integer(compute='_compute_book_count')
 
     def _compute_book_count(self):
-        # pass
-        all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-        all_partners.read(['parent_id'])
-
-        book_order_groups = self.env['library.book'].read_group(
-            domain=[('partner_id', 'in', all_partners.ids)],
+        books_data = self.env['library.book'].read_group(
+            domain=[('partner_id', 'in', self.ids)],
             fields=['partner_id'], groupby=['partner_id']
         )
-        partners = self.browse()
-        for group in book_order_groups:
+        self.book_count = 0
+        for group in books_data:
             partner = self.browse(group['partner_id'][0])
-            while partner:
-                if partner in self:
-                    partner.book_order_count += group['partner_id_count']
-                    partners |= partner
-                partner = partner.parent_id
-        (self - partners).book_order_count = 0
+            partner.book_count = group['partner_id_count']
 
+    def action_view_books(self):
+        self.ensure_one()
+        return {
+            'name': _('Partners Books'),
+            'view_mode': 'tree,form',
+            'res_model': 'library.book',
+            'type': 'ir.actions.act_window',
+            'context': {'search_default_partner_id': self.id}
+        }
