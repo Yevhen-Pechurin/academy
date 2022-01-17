74809f9b91aa8b4df8dfbb1f904887b8b9bd917e
OlgaGorbenko <olga.gorbenko@gmail.com>
2022-01-17 15:16:11 +0200

    module install

---
+++ rental/models/car.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Model(models.Model):
+    _name = 'rental.model'
+    _description = 'Model of car'
+
+    name = fields.Char()
+
+
+class Country(models.Model):
+    _name = 'rental.country'
+    _description = 'Country of origin'
+
+    name = fields.Char()
+
+
+class History(models.Model):
+    _name = 'rental.history'
+    _description = 'History'
+
+    car_id = fields.Many2one('rental.car')
+    partner_id = fields.Many2one('res.partner')
+    date_on_rent = fields.Date()
+    date_in_garage = fields.Date()
+    due_date = fields.Date()
+
+
+class CarInfo(models.Model):
+    _name = 'rental.car.info'
+    _inherit = 'mail.thread'
+    _description = 'Car Info'
+
+    name = fields.Char(tracking=True)
+    model_id = fields.Many2one('rental.model', tracking=True)
+    country_id = fields.Many2one('rental.country', tracking=True)
+    description = fields.Text(tracking=True)
+
+
+class Car(models.Model):
+    _name = 'rental.car'
+    _inherit = 'mail.thread'
+    _description = 'Car'
+
+    car_id = fields.Many2one('rental.car.info')
+    name = fields.Char(related='car_id.name', readonly=False)
+    number = fields.Char(copy=False)
+    model_id = fields.Many2one(related='car_id.model_id')
+    year = fields.Integer()
+    mileage = fields.Integer()
+    country_id = fields.Many2one(related='car_id.country_id')
+
+
+#     # status = fields.Selection([
+#     #     ('in_garage', 'In Garage'),
+#     #     ('on_rent', 'On Rent'),
+#     #     ('unavailable', 'Unavailable'),
+#     # ], default='in_garage', compute='_compute_status', store=True, tracking=True)
+#     partner_id = fields.Many2one('res.partner')
+#     history_ids = fields.One2many('rental.history', 'car_id')
+#     image = fields.Image(string="Image", max_width=256, max_height=256, help="Select image here", verify_resolution=True)
+#     description = fields.Text(related='car_id.description')
+#     # due_date = fields.Date()
+#     # overdue_notification_date = fields.Date()
+#     # active = fields.Boolean(default=True)
+#
+#     _sql_constraints = [
+#         ('number_unig', 'unique (number)', """Only one number can be defined for each car!""")
+#     ]
+
+
+
+
