22e39d66a2a4a9e3227c191bd916693d529cf146
Ievgen <sizority@gmail.com>
2022-01-17 20:18:12 +0200

    Button "Create" work. Сorrected field "number".

--- rental_car/models/car.py
+++ rental_car/models/car.py
@@ -11,8 +11,8 @@ class Car(models.Model):
     _description = 'Rental Car'
 
     name = fields.Char()
-    number = fields.Integer()
-    model = fields.Many2one()
+    number = fields.Char()
+    # model = fields.Many2one()
     year = fields.Integer()
     # status = fields.Selection([
     #     ('in_garage', 'In Garage'),
@@ -21,4 +21,17 @@ class Car(models.Model):
     #     ('unavailable', 'Unavailable'),
     # ], default='in_garage', compute='_compute_status',
     #     store=True, tracking=True)
+    lease_data = fields.Date()
+    # partner_id = fields.Many2one('res.partner')
+    # history_ids = fields.One2many()
+    logo_image = fields.Image(string="Image", max_width=256, max_height=256)
+    odometer = fields.Integer()
+    active = fields.Boolean(default=True)
 
+    @api.depends('active')
+    def _compute_status(self):
+        for car in self:
+            if not car.active:
+                car.status = 'rented'
+            else:
+                car.status = car.status
