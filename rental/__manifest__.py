f3d8ecef65d3cd05f1c8962bb8d7396f0cb47ca5
inspiration679 <taraskalaida@gmail.com>
2022-01-17 13:59:56 +0200

    Init

---
+++ rental/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Rental',
+    'version': '1.0',
+    'summary': 'Test Car Rental',
+    'description': """Test module <Car Rental> made by Inspiration679""",
+    'category': 'Cars',
+    'website': 'https://github.com/Inspiration679',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/car_rental_view.xml',
+        'wizards/wizard_loan.xml',
+        'data/ir_cron.xml'
+    ],
+    'demo': [
+        'demo/car_rental_demo.xml',
+    ],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
