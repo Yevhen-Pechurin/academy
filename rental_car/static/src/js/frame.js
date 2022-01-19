a6ad3d71d08811cd22c870a2eae3eea05cb09e40
Ievgen <sizority@gmail.com>
2022-01-19 03:22:49 +0200

    Add 3 buttons with js.

--- rental_car/static/src/js/frame.js
+++ rental_car/static/src/js/frame.js
@@ -18,4 +18,43 @@ odoo.define('rental_car.frame', function (require) {
         },
     });
 
+    const Widget = require('web.Widget');
+
+    const Counter = Widget.extend({
+        template: 'rental_car.car',
+        events: {
+            'click .add_car_btn': '_add',
+            'click .remove_car_btn': '_remove',
+            'multiply_car_btn': '_multiply',
+        },
+        init: function (parent, value) {
+            this._super(parent);
+            this.count = value;
+        },
+        _add: function () {
+            this.count++;
+            this.$('.val').text(this.count);
+        },
+        _remove: function () {
+            setInterval(() => {
+                this.count++;
+                this.$('.val').text(this.count);
+            }, 100)
+        },
+        _multiply: function () {
+            setInterval(() => {
+                this.count++;
+                this.$('.val').text(this.count);
+            }, 100)
+        }
+    });
+
+    require('web.dom_ready')
+    var counter = new Counter(this, 4);
+// Render and insert into DOM
+    counter.appendTo("body");
+
+    // return Animal;
+
+    return {Frame, Counter};
 })
