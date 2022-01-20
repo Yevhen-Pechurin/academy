6b9d4803a8b9b120e89ca5279a5c0f56d7054925
denysmedvedskyi <medvedskyi.denys@gmail.com>
2022-01-20 20:00:28 +0200

    add field widget(don`t work!)

--- rental/static/src/js/car.js
+++ rental/static/src/js/car.js
@@ -1,5 +1,7 @@
 odoo.define('rental.car', function(require) {
             'use strict';
+            const core = require('web.core');
+            const QWeb = core.qweb;
 
             const FieldChar = require('web.basic_fields').FieldChar;
 
@@ -19,10 +21,21 @@ odoo.define('rental.car', function(require) {
                             method: 'get_model',
                             args: [query],
                         }).then((data) => {
-                            console.log(data)
+                            this.data_model = data
+                            if (data && data.length > 0) {
+                                const elem = $(QWeb.render('rental.model_list', {
+                                    list_search: data,
+                                    }));
+                                elem.find('.list_car_span').on('click', self._onClickModel.bind(self));
+//                                self.$el.after(elem);
+                                self.appendTo("body");
+                                }
                             //            console.log(data)
                         })
-                    }
+                    },
+                    _onClickModel: function(self) {
+                    console.log(this.data_model)
+                }
             })
 
             fieldRegistry.add('my-custom-field', CustomFieldChar);
