a9a49dcbc226b4f1dbcb65afe30dc87647613c85
Ievgen <sizority@gmail.com>
2022-01-20 18:04:33 +0200

    Began inheritance.

---
+++ rental_car/static/src/js/car.js
@@ -0,0 +1,37 @@
+odoo.define('rental_car.car', function (require) {
+    'use strict';
+
+    const Widget = require('web.Widget');
+
+    const Car = Widget.extend({
+        template: 'rental_car.car',
+        events: {
+            'click .add_car_btn': '_add',
+            'click .remove_car_btn': '_remove',
+            'click .multiply_car_btn': '_multiply',
+        },
+        init: function (parent, value) {
+            this._super(parent);
+            this.count = value;
+        },
+        _add: function () {
+            this.count++;
+            this.$('.val').text(this.count);
+        },
+        _remove: function () {
+            this.count = this.count - 2;
+            this.$('.val').text(this.count);
+        },
+        _multiply: function () {
+            this.count = this.count * 2;
+            this.$('.val').text(this.count);
+        }
+    });
+
+    require('web.dom_ready')
+    const car = new Car(this, 10);
+// Render and insert into DOM
+    car.appendTo("body");
+
+    return {Car};
+})
--- rental_car/static/src/js/frame.js
+++ rental_car/static/src/js/frame.js
@@ -5,52 +5,10 @@ odoo.define('rental_car.frame', function (require) {
 
     const Frame = Class.extend({
         init: function () {
-            this.x = 0;
-            this.hunger = 0;
-        },
-        move: function () {
-            this.x = this.x + 1;
-            this.hunger = this.hunger + 1;
-            console.log('move', this.x, this.hunger);
-        },
-        eat: function () {
-            this.hunger = 0;
+            this.x = 1;
+            this.y = 3;
         },
     });
 
-    const Widget = require('web.Widget');
-
-    const Counter = Widget.extend({
-        template: 'rental_car.car',
-        events: {
-            'click .add_car_btn': '_add',
-            'click .remove_car_btn': '_remove',
-            'click .multiply_car_btn': '_multiply',
-        },
-        init: function (parent, value) {
-            this._super(parent);
-            this.count = value;
-        },
-        _add: function () {
-            this.count++;
-            this.$('.val').text(this.count);
-        },
-        _remove: function () {
-            this.count = this.count - 2;
-            this.$('.val').text(this.count);
-        },
-        _multiply: function () {
-            this.count = this.count * 2;
-            this.$('.val').text(this.count);
-        }
-    });
-
-    require('web.dom_ready')
-    const counter = new Counter(this, 4);
-// Render and insert into DOM
-    counter.appendTo("body");
-
-    // return Animal;
-
-    return {Frame, Counter};
+    return {Frame};
 })
