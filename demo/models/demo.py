c1fabb75d1a21bd6672f32a246b13eb2ea0399ec
Ievgen <sizority@gmail.com>
2022-01-24 14:00:44 +0200

    Add  user_id.

--- demo/models/demo.py
+++ demo/models/demo.py
@@ -8,22 +8,33 @@ class Demo(models.Model):
     # _inherit = 'mail.thread'
     _description = 'Demo'
 
-    name = fields.Char()
-    user_id = fields.Char()
-    partner_id = fields.Many2one('res.partner')
-    date = fields.Date()
+    name = fields.Char(tracking=True)
+    user_id = fields.Many2one('res.users', string='User Id', tracking=True,  default=lambda self: self.env.user)
+    partner_id = fields.Many2one('res.partner', tracking=True)
+    date = fields.Date(tracking=True)
     state = fields.Selection([
         ('scheduled', 'Scheduled'),
         ('in_progres', 'In Progres'),
         ('done', 'Done'),
         ('сanceled', 'Canceled'),
     ], default='scheduled', compute='_compute_state', store=True, tracking=True)
-    description = fields.Text()
+    description = fields.Text(tracking=True)
     active = fields.Boolean(default=True)
 
     _sql_constraints = [
         ('name_unig', 'unique (name)', """Only one name can be defined for each user!""")
     ]
+
+    # def action_in_progres(self):
+    #     return {
+    #         'name': _('In Progres %s') % self.name,
+    #         'view_mode': 'form',
+    #         'res_model': 'rental.wizard.on_rent',
+    #         'type': 'ir.actions.act_window',
+    #         'target': 'new'
+    #     }
+
+
 
     @api.depends('active')
     def _compute_state(self):
