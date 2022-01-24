f13e95977421cc148c95488af8ab21c485aed87c
Valentin <wal2004yandex.ru@gmail.com>
2022-01-24 14:07:41 +0200

    Try to fix

--- demo/wizards/wizard_demo.py
+++ demo/wizards/wizard_demo.py
@@ -2,7 +2,7 @@ from odoo import fields, models
 
 
 class WizardDemo(models.TransientModel):
-    _name = 'demo.wizard.demo'
+    _name = 'demo.wizard.demos'
 
     _description = 'Wizard for user'
 
