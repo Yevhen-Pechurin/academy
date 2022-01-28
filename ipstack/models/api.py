60e44b0b540e8db6f060d25d329f6f786b47962b
Valentin <wal2004yandex.ru@gmail.com>
2022-01-28 12:15:18 +0200

    Init

--- ipstack/models/api.py
+++ ipstack/models/api.py
@@ -1,21 +1,24 @@
 import logging
 import requests
 
-URL = "http://api.ipstack.com/"
-
 _logger = logging.getLogger(__name__)
 
 
 class IpStackAPI(object):
 
-    def __init__(self, key, url=URL):
+    def __init__(self, key, url=URL, timeout=10):
         self.url = url
         self.key = key
+        self.timeout = timeout
 
     def get_ip_data(self, ip):
-        data = request
-        return data
+        url = self.join_url(self.url, ip)
+        response = requests.get(url, params={'access_key': self.key}, timeout=self.timeout)
+        response.raise_for_status()
+        return response.json()
 
+    def join_url(self, *args):
+        return "/".join(arg.strip("/") for arg in args)
 
 if __name__ == "__main__":
     api = IpStackAPI('f9f17c8a2e83b94a4dab3115b4d64698')
--- ipstack/models/models.py
+++ ipstack/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+from .api import IpStackAPI
 
 
-# class ipstack(models.Model):
-#     _name = 'ipstack.ipstack'
-#     _description = 'ipstack.ipstack'
+class IpStack(models.Model):
+    _name = 'ipstack.api'
+    _description = 'ipstack.ipstack'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    def get_api(self):
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        url = get_param('ip.stack.url', 'http://api.ipstack.com/')
