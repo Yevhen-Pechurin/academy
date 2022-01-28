481efc91f2af050fad47d337d58545f68e492299
Valentin <wal2004yandex.ru@gmail.com>
2022-01-28 17:22:43 +0200

    [DRAFT] In progress

--- ipstack/models/ipstack_api.py
+++ ipstack/models/ipstack_api.py
@@ -8,11 +8,6 @@ class IpStack(models.Model):
     _name = 'ipstack.api'
     _description = 'ipstack.ipstack'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
     def get_api(self):
         get_param = self.env['ir.config_parameter'].sudo().get_param
         url = get_param('ip.stack.url', 'http://api.ipstack.com/')
