3e441ee8ebaaef581c7c5c5092b5968be6153fb4
Valentin <wal2004yandex.ru@gmail.com>
2022-02-01 18:29:32 +0200

    Change assert

--- rental/tests/test_car.py
+++ rental/tests/test_car.py
@@ -20,9 +20,10 @@ class TestCar(TestCarCommonBase):
 
     def test_action_garage(self):
         self.assertEqual(self.car_1.status, 'on_loan', 'Car status is not correct\nStatus:%s' % self.car_1.status)
+        self.assertTrue(self.car_1.partner_id, 'Car partner did not take off \nStatus:%s' % self.car_1.partner_id)
         self.car_1.action_repair()
         self.assertEqual(self.car_1.status, 'under_repair', 'Car status is not correct\nStatus:%s' % self.car_1.status)
-        self.assertEqual(self.car_1.partner_id, False, 'Car partner did not take off \nStatus:%s' % self.car_1.partner_id)
+        self.assertFalse(self.car_1.partner_id, 'Car partner did not take off \nStatus:%s' % self.car_1.partner_id)
 
     def test_on_loan(self):
         self.assertEqual(self.car_1.status, 'on_loan', 'Car status is not correct\nStatus:%s' % self.car_1.status)
