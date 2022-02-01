9df13f0911c0a949f9fe8c2e3cc80fab3c4b3f42
Xpshnik <turnikmen2011@gmail.com>
2022-02-01 16:50:53 +0200

    Working model creation tour

--- rental/static/src/js/tours/rental.js
+++ rental/static/src/js/tours/rental.js
@@ -21,5 +21,41 @@ tour.register('rental_tour', {
     trigger: '.dropdown-item[data-menu-xmlid="rental.display_all_models_menu_action"]',
     content: Markup(_t('Let\'s see what models are already in catalogue!')),
     position: 'right'
+}, {
+    trigger: '.o_list_button_add',
+    extra_trigger: '.o_rental_car_model_tree',
+    content: Markup(_t('Create your first model!')),
+    position: 'right'
+}, {
+    trigger: ".o_form_view .o_field_widget[name='model_name']",
+    content: Markup(_t('Write your model name!')),
+    position: 'top',
+    run: function(actions) {
+        actions.text('ХС40', this.$anchor);
+    }
+}, {
+    trigger: ".o_form_view .o_field_widget[name='manufacturer_id']",
+    content: Markup(_t('Write a trademark name to get prompted.')),
+    position: 'left',
+    run: function(actions) {
+        actions.text('Volvo', this.$anchor.find('input'));
+    }
+}, {
+    trigger: ".ui-menu-item > a",
+    auto: true,
+    in_modal: false
+}, {
+    trigger: '.o_form_button_save',
+    extra_trigger: '.o_rental_car_model_form',
+    content: Markup(_t('Save your first model.')),
+    position: 'right'
+}, {
+    mobile: false,
+    trigger: '.breadcrumb-item:first',
+    content: Markup(_t('Use the breadcrumbs to go back.')),
+    position: 'bottom',
+    run: function(actions) {
+        actions.auto('.breadcrumb-item:not(.active)');
+    }
 }
 ])
