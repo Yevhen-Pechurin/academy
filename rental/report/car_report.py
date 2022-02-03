2e386f4408641060ee30edac23a0ac38378873ce
Xpshnik <turnikmen2011@gmail.com>
2022-02-03 20:17:57 +0200

    field name fixes

--- rental/report/car_report.py
+++ rental/report/car_report.py
@@ -10,7 +10,7 @@ class CarReport(models.Model):
     year = fields.Integer()
     #rental_history_id = fields.Many2one('rental.history')
     #maintenance_history_id = fields.Many2one('rental.maintenance_history')
-    client_id = fields.Many2one('res.partner')
+    rentee_id = fields.Many2one('res.partner')
 
     max_year = fields.Integer(group_operator='max')
     
@@ -22,7 +22,7 @@ class CarReport(models.Model):
             rc.id car_id,
             COALESCE (rcm.model_name, 'unknown model') car_model_name,
             rc.year AS year,
-            client_id,
+            rentee_id,
             COALESCE (year, 0) max_year
         FROM rental_car rc
         LEFT JOIN rental_car_model rcm ON rc.model = rcm.model_name
