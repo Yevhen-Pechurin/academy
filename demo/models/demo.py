243789c09c918ffb995e046f56df82a04b30dab7
whatisevol <medvedskyi.denys@gmail.com>
2022-02-04 12:25:44 +0200

    [DEV] add tree view

--- demo/models/demo.py
+++ demo/models/demo.py
@@ -5,20 +5,23 @@ from odoo import models, fields, api
 
 class Demo(models.Model):
     _name = 'demo.demo'
+    _inherit = 'mail.thread'
     _description = 'demo.demo'
 
     name = fields.Char()
-    salesperson = fields.Many2one('res.users', copy=False, tracking=True,
-                                      string='Salesperson',
-                                      default=lambda self: self.env.user)
-    description = fields.Text()
+    salesperson = fields.Many2one('res.users',
+                                  copy=False,
+                                  tracking=True,
+                                  string='Salesperson',
+                                  default=lambda self: self.env.user)
     partner_id = fields.Many2one('res.partner', string='Client', tracking=True)
     date = fields.Date(tracking=True)
-    status = fields.Selection([
+    state = fields.Selection([
         ('scheduled', 'Scheduled'),
         ('in_progress', 'In Progress'),
         ('done', 'Done'),
         ('canceled', 'Canceled'),
     ], default='scheduled', tracking=True)
     description = fields.Text(tracking=True)
+
 
