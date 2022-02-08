1201fa53fd0406323080a545cfe232da34af2a9d
OlgaGorbenko <olga.gorbenko@gmail.com>
2022-02-08 03:09:42 +0200

    add field country to product and order

--- sale_origin/__manifest__.py
+++ sale_origin/__manifest__.py
@@ -10,7 +10,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -18,8 +18,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/sale_origin_views.xml',
+        # 'security/ir.model.access.csv',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- sale_origin/models/sale_origin.py
+++ sale_origin/models/sale_origin.py
@@ -5,6 +5,10 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    country_of_origin = fields.Char()
+    country_of_origin = fields.Char(string='Country of origin')
 
 
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    country_of_origin = fields.Char(string='Country of origin')
