9dbb5f7b674a94bc7b0587dbbad5c82d30cb62af
denysmedvedskyi <medvedskyi.denys@gmail.com>
2022-02-11 19:13:54 +0200

    add some tests

--- zoom_info/tests/test_zoom_info.py
+++ zoom_info/tests/test_zoom_info.py
@@ -4,10 +4,10 @@ from odoo.exceptions import ValidationError
 from odoo.tests import tagged, Form, HttpCase, TransactionCase
 from odoo import fields
 from odoo.addons.zoom_info.models.zoom_info import ZoomInfo
-from ..models.zoom_api import ZoomApi
 from odoo.addons.website.tools import MockRequest
 from unittest import mock
 from odoo.addons.zoom_info.models.zoom_api import ZoomApi
+import requests
 
 SEARCH_COMPANY = {'maxResults': 1110, 'totalResults': 25, 'currentPage': 1,
                   'data': [{'id': 104333869, 'name': 'Tesla'},
@@ -32,17 +32,25 @@ SEARCH_COMPANY = {'maxResults': 1110, 'totalResults': 25, 'currentPage': 1,
                            {'id': 407004731, 'name': 'Nikola Tesla Educational Corporation'},
                            {'id': 455952800, 'name': 'ecar-rent'},
                            {'id': 373839676, 'name': 'Tesla InfoTech'},
-                           {'id': 346396693, 'name': 'Tesla Transformers'}]}
+                           {'id': 346396693, 'name': 'Tesla Transformers'}],
+                  'error': ''}
 
 ENRICH_COMPANY = {'success': True, 'data': {'outputFields': [
-            ['name', 'street', 'city', 'state', 'zipcode', 'country', 'phone', 'website', 'logo', 'id']],
-            'result': [{'input': {'companyid': '104333869'}, 'data': [
-                {'name': 'Tesla', 'street': '13101 Harold Green Road',
-                 'city': 'Austin', 'state': 'Texas', 'zipCode': '78725',
-                 'country': 'United States', 'phone': '(650) 681-5000',
-                 'website': 'www.tesla.com',
-                 'logo': 'https://res.cloudinary.com/zoominfo-com/image/upload/w_100,h_100,c_fit/tesla.com',
-                 'id': 104333869}], 'matchStatus': 'FULL_MATCH'}]}}
+    ['name', 'street', 'city', 'state', 'zipcode', 'country', 'phone', 'website', 'logo', 'id']],
+    'result': [{'input': {'companyid': '104333869'}, 'data': [
+        {'name': 'Tesla', 'street': '13101 Harold Green Road',
+         'city': 'Austin', 'state': 'Texas', 'zipCode': '78725',
+         'country': 'United States', 'phone': '(650) 681-5000',
+         'website': 'www.tesla.com',
+         'logo': 'https://res.cloudinary.com/zoominfo-com/image/upload/w_100,h_100,c_fit/tesla.com',
+         'id': 104333869}], 'matchStatus': 'FULL_MATCH'}]}}
+
+ENRICH_CONTACT = {'success': True, 'data': {'outputFields': [
+    ['name', 'street', 'city', 'state', 'zipcode', 'country', 'phone', 'website', 'logo', 'id']],
+    'result': [{'input': {'companyid': '104333869'}, 'data': [
+        {'name': 'Tesla',
+         'image': "",
+         }], 'matchStatus': 'FULL_MATCH'}]}}
 
 
 def _zoom_info_request(self, url, params=None):
@@ -64,12 +72,27 @@ def mocked_requests_post(*args, **kwargs):
         def json(self):
             return self.json_data
 
-    if args[0] == 'https://api.zoominfo.com/authenticate':
+        def raise_for_status(self):
+            """Raises stored :class:`HTTPError`, if one occurred."""
+
+            http_error_msg = ''
+            if 400 <= self.status_code < 500:
+                http_error_msg = u'%s Client Error: %s for url: %s' % (self.status_code, self.url)
+
+            elif 500 <= self.status_code < 600:
+                http_error_msg = u'%s Server Error: %s for url: %s' % (self.status_code, self.url)
+
+            if http_error_msg:
+                raise requests.HTTPError(http_error_msg, response=self)
+
+    if kwargs['url'] == 'https://api.zoominfo.com/authenticate/':
         return MockResponse({'jwt': "Token123"}, 200)
-    elif args[0] == 'https://api.zoominfo.com/search/company':
+    elif kwargs['url'] == 'https://api.zoominfo.com/search/company/':
         return MockResponse(SEARCH_COMPANY, 200)
-    elif args[0] == 'https://api.zoominfo.com/enrich/company':
+    elif kwargs['url'] == 'https://api.zoominfo.com/enrich/company/':
         return MockResponse(ENRICH_COMPANY, 200)
+    elif kwargs['url'] == 'https://api.zoominfo.com//enrich/contact/':
+        return MockResponse(ENRICH_CONTACT, 200)
 
     return MockResponse(None, 401)
 
@@ -78,11 +101,10 @@ def mocked_requests_post(*args, **kwargs):
 class TestZoomJs(HttpCase):
     def setUp(self):
         super(TestZoomJs, self).setUp()
-        self.patch(ZoomInfo, 'zoom_info_request', _zoom_info_request)
+        # self.patch(ZoomInfo, 'zoom_info_request', _zoom_info_request)
 
-    # @mock.patch('odoo.addons.zoom_info.models.zoom_api.requests.post', side_effect=mocked_requests_post)
-    # def test_tour(self, mock_post):
-    def test_tour(self):
+    @mock.patch('odoo.addons.zoom_info.models.zoom_api.requests.request', side_effect=mocked_requests_post)
+    def test_tour(self, mock_post):
         self.start_tour("/web", 'zoom_info_tour', login='admin', timeout=180)
         partner = self.env['res.partner'].search([('name', '=', 'Tesla')], limit=1)
         self.assertItemsEqual(partner.name, 'Tesla')
@@ -94,17 +116,15 @@ class TestZoomJs(HttpCase):
         self.assertItemsEqual(partner.phone, '(650) 681-5000')
         self.assertItemsEqual(partner.website, self.env['res.partner']._clean_website('www.tesla.com'))
 
-
 @tagged('zoom_info')
 class TestZoomInfo(TransactionCase):
     def setUp(self):
         super(TestZoomInfo, self).setUp()
         self.zoom_info_test = self.env['zoom.info'].create([])
-        self.patch(ZoomInfo, 'zoom_info_request', _zoom_info_request)
+        # self.patch(ZoomInfo, 'zoom_info_request', _zoom_info_request)
         self.maxDiff = None
 
-
-    # @mock.patch('odoo.addons.zoom_info.models.zoom_info.requests.post', side_effect=mocked_requests_post)
+    @mock.patch('odoo.addons.zoom_info.models.zoom_api.requests.request', side_effect=mocked_requests_post)
     def test_creation(self, mock_post):
         companies = self.zoom_info_test.find_company_id('Tesla')
         expected_companies = [{'id': 104333869, 'name': 'Tesla'},
