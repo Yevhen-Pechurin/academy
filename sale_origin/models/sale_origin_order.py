7b6a282cb334eb1a36ac9c358592440a70385641
Valentin <wal2004yandex.ru@gmail.com>
2022-02-14 15:43:42 +0200

    Init overwrite method for add field temp in description

--- sale_origin/models/sale_origin_order.py
+++ sale_origin/models/sale_origin_order.py
@@ -3,8 +3,10 @@ from odoo import models, fields, api
 
 
 class SaleOrderLine(models.Model):
-    _description = 'sale_origin.line'
     _inherit = 'sale.order.line'
+
     country_of_origin = fields.Char(string='Country of Origin', related='product_id.country_of_origin')
     temperature_regime = fields.Char(related='product_id.temperature_regime')
 
+    def get_sale_order_line_multiline_description_sale(self, product):
+        return super(SaleOrderLine, self).get_sale_order_line_multiline_description_sale(product) + f'\nTemperature Regime: {self.temperature_regime}'
